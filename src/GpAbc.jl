740ea313b2ebef2a0b049229f0eb12693d49d659
Evgeny Tankhilevich <et517@ic.ac.uk>
2018-06-15 17:22:52 +0100

    ABaCus integration

--- src/GpAbc.jl
+++ src/GpAbc.jl
@@ -14,23 +14,47 @@ export
 	scaled_squared_distance, scaled_squared_distance_grad,
 	get_hyperparameters_size, set_hyperparameters,
     gp_train,
-	multiple_training_abc,
-	multiple_training_seq_abc,
-	smc,
-	get_head_of_ref_table,
-	rejection_abc;
+	# multiple_training_abc,
+	# multiple_training_seq_abc,
+	# smc,
+	# get_head_of_ref_table,
+	# rejection_abc,
+
+	ABCrejection,
+	initialiseABCSMC, iterateABCSMC!, ABCSMC,
+	ABCInput, ABCOutput,
+	ABCRejectionInput, ABCSMCInput,
+	SimulatedABCRejectionInput, EmulatedABCRejectionInput,
+	SimulatedABCSMCInput, EmulatedABCSMCInput,
+	ABCRejectionOutput, ABCSMCOutput,
+	ABCSMCTracker,
+	SimulatedABCSMCTracker, EmulatedABCSMCTracker,
+	read_rejection_output, read_smc_output;
 
 using Optim, Distributions
 
-include("kernels/scaled_squared_distance.jl")
-include("kernels/abstract_kernel.jl")
-include("kernels/rbf_kernels.jl")
-include("kernels/matern_kernels.jl")
-include("gp.jl")
-include("abc.jl")
-include("gp_optimisation.jl")
-include("multiple_training_abc.jl")
-include("mv_uniform.jl")
-include("latin_hypercube_sampler.jl")
+# import Distances
+import Distributions
+import StatsBase
+
+import Base: write
+
+
+include("gp/kernels/scaled_squared_distance.jl")
+include("gp/kernels/abstract_kernel.jl")
+include("gp/kernels/rbf_kernels.jl")
+include("gp/kernels/matern_kernels.jl")
+include("gp/gp.jl")
+include("gp/gp_optimisation.jl")
+include("util/mv_uniform.jl")
+include("util/latin_hypercube_sampler.jl")
+
+include("abc/io.jl")
+include("abc/rejection.jl")
+include("abc/smc.jl")
+
+
+# include("abc.jl")
+# include("multiple_training_abc.jl")
 
 end;
