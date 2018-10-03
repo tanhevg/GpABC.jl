3d0b070539b8414b7efa17551f3eee684f7d8ef2
Tara Hameed <tah17@ic.ac.uk>
2018-10-03 15:31:16 +0100

    adding in abc tests to lna

--- test/lna-test.jl
+++ test/lna-test.jl
@@ -4,7 +4,7 @@ using Base.Test, GpABC, DifferentialEquations, Distributions, Distances
 
     params = [2.0, 1.0, 15.0, 1.0, 1.0, 1.0, 100.0, 1.0, 1.0, 1.0]
     Tspan = (0.0, 10.0)
-    volume = 1.0
+    volume = 100.0
     n_samples = 10
     n_design_points = 200
     priors = [Uniform(0., 5.), Uniform(0., 5.), Uniform(10., 20.)]
@@ -28,7 +28,7 @@ using Base.Test, GpABC, DifferentialEquations, Distributions, Distances
     end
 
     #LNA Mean Var example
-    x0 = ([0.0, 0.0, 0.0], 0.4*eye(3,3))
+    x0 = ([3.0, 2.0, 1.0], 0.4*eye(3,3))
     input = LNAInput(params, S, reaction_rate_function, volume)
     lna = compute_LNA(input, x0, Tspan, saveat)
     @test size(lna.traj_means,2) == length(lna.time_points)
@@ -44,5 +44,37 @@ using Base.Test, GpABC, DifferentialEquations, Distributions, Distances
 
     @test size(X,1) == n_design_points
     @test size(X,2) == length(priors)
+
+    #ABC-SMC simulation with LNA
+    threshold_schedule = [4.0, 3.0, 2.0]
+    n_samples = 100
+    n_particles = 500
+    n_var_params = length(priors)
+    reference_data = sample_LNA_trajectories(lna, n_samples)
+
+    function simulator_function(var_params)
+        input = LNAInput(vcat(var_params, params[n_var_params+1:end]), S, reaction_rate_function, volume)
+        return get_LNA_trajectories(input, n_samples, x0, Tspan, saveat)
+    end
+
+    sim_abcsmc_res = SimulatedABCSMC(reference_data,
+    n_particles,
+    threshold_schedule,
+    priors,
+    "keep_all",
+    simulator_function)
+
+    @test size(sim_abcsmc_res.population, 1) > 0
+
+    #ABC-SMC emulation with LNA
+
+    emu_abcsmc_res = EmulatedABCSMC(n_design_points, reference_data,
+    n_particles,
+    threshold_schedule,
+    priors,
+    "keep_all",
+    simulator_function)
+
+    @test size(emu_abcsmc_res.population, 1) > 0
 
 end
